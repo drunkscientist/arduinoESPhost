9034084a276322a952a1580a24ec60701b414000
Robert Eells <eellsrm@gmail.com>
2019-07-05 19:03:52 -0400

    Add files via upload

--- serialMediator.ino.ino
+++ serialMediator.ino.ino
@@ -1,12 +1,15 @@
-const int led = 11;
+const int led = 13;
 String networkName;
 String pswd;
+String cmnd;
+bool passthru;
 
 void setup() {
  Serial.begin(9600);
  Serial1.begin(9600);
  pinMode(led, OUTPUT);
  digitalWrite(led, LOW);
+ passthru = false;
 
 
  
@@ -14,7 +17,13 @@ void setup() {
 
 void loop() {
   frWifi();//from wifi
+  if (passthru = true){     
   toWifi();//to wifi
+  }
+  else{
+    if (Serial.available())
+      Serial1.write(Serial.read());
+  }
 }
 
 void frWifi(){
@@ -28,7 +37,7 @@ void frWifi(){
 }
 
 void toWifi(){
- if (Serial.available()>0){ //ifReadFromPC
+ if (Serial.available()>0){ //if there is input from the pc
   char command = Serial.read();
 /**/
   switch (command){
@@ -39,10 +48,13 @@ void toWifi(){
       digitalWrite(led, !digitalRead(led));
      break;
     case '2':
-      Serial1.println("AT+CIFSR=1"); //show IP address
+      Serial1.println("AT+CIFSR"); //show IP address
+      if (Serial1.available()>0){
+        Serial.write(Serial1.read());
+      }
      break;
     case '3':
-      //Serial.println("AT+CWMODE=3");
+      Serial.println("AT+CWMODE=3");
      break;
     case '4':
       Serial1.println("AT+CIPMUX=1"); //enable multiple connections
@@ -50,10 +62,24 @@ void toWifi(){
     case '5':
       Serial1.println("AT+CIPSERVER=1,80"); //start tcp server
      break;
+    case '6':
+      Serial1.println("AT+CIPSERVER=1,1336"); //start tcp server
+     break;
+
+
      
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/     
+    
+    
     case 'c':
     case 'C':
-      Serial.println("turn off line ending and enter SSID:");
+    Serial.println("turn off line ending");
+    delay(7000);
+    if (Serial.available()>0){
+      char z = Serial.read();
+      delay(10);
+    }
+      Serial.println("enter SSID:");
        while (Serial.available()==0){delay(5000);}//While serial buffer is empty, do nothing
        networkName = Serial.readString();
       Serial.println("enter password:");
@@ -61,7 +87,7 @@ void toWifi(){
        pswd =  Serial.readString();
       Serial.println("power on esp...");
       while (Serial.available()==0){delay(1000);}
-      String cmnd = "AT+CWJAP=\"";
+      cmnd = "AT+CWJAP_CUR=\""; //change CUR to DEF to make network permanant
       cmnd += networkName;
       cmnd += "\",\"";
       cmnd += pswd;
@@ -69,9 +95,16 @@ void toWifi(){
       Serial.println(cmnd); //changing this to Serial1 will send the generated command to the esp
             
      break;
-    }
-  
-        
+
+    case 'r':
+    case 'R':
+      Serial1.println("AT+RST");
+     break;
+
+    default:
+      
+     break;
+    }        
   }
 } 
 
